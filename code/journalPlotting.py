7b7252bcef1562c0e6e68234ac938d06187f8d28
Adele Kapellusch <akoutia@email.arizona.edu>
2018-03-10 12:30:42 -0700

    Updated plotting

--- code/journalPlotting.py
+++ code/journalPlotting.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
+
 
 # Load Data
 journalData = pd.read_csv('../data/Journals.csv')
@@ -16,7 +18,8 @@ plt.tight_layout()
 plt.savefig('../plots/nAuthorsBoxplot.png')
 plt.show()
 
-
+# Create Joint Plot
 joint = sns.jointplot('Eigenfactor_Score', 'Journal_Impact_Factor', kind="kde", size=7, space=0, data=journalData)
 plt.savefig('../plots/EigenImpactJointplot.png')
 plt.show()
+
