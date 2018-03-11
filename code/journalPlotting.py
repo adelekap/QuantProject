976694cc854b3f8f53b9256ac17f6fde5876ecab
Adele Kapellusch <akoutia@email.arizona.edu>
2018-03-10 18:15:40 -0700

    Updated

--- code/journalPlotting.py
+++ code/journalPlotting.py
@@ -8,13 +8,14 @@ import numpy as np
 journalData = pd.read_csv('../data/Journals.csv')
 articleData = pd.read_csv('../data/Articles.csv')
 
-# Make open_access field binary category
+
 journalData.open_access = pd.Categorical(journalData.open_access)
 journalData['open_access'] = journalData.open_access.cat.codes
 
 # Create Box Plot
 box = sns.boxplot('journal_id','n_authors',data=articleData)
 plt.tight_layout()
+plt.title('Number of Authors')
 plt.savefig('../plots/nAuthorsBoxplot.png')
 plt.show()
 
@@ -23,3 +24,8 @@ joint = sns.jointplot('Eigenfactor_Score', 'Journal_Impact_Factor', kind="kde", 
 plt.savefig('../plots/EigenImpactJointplot.png')
 plt.show()
 
+# Create Distribution
+dist = sns.distplot(articleData['n_pages'])
+plt.title('Number of Pages')
+plt.savefig('../plots/nPages.pdf')
+plt.show()
